48948969414109c2120adaf2aeddbb0a3bda18a6
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-11-05 11:21:11 +0700

    feat: make bar looks more clearly

--- components/TopBar.jsx
+++ components/TopBar.jsx
@@ -1,27 +1,41 @@
-// TopBar.js
 import React from 'react';
-import {View, StyleSheet, StatusBar} from 'react-native';
+import { View, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const TopBar = ({
                     leftComponent,
                     centerComponent,
                     rightComponent,
                     iconContainerStyle,
+                    leftFlex = 1,
+                    centerFlex = 3,
+                    rightFlex = 1,
+                    onLeftPress, // Add a prop for handling left component press
                 }) => {
+    const isArrowIcon = React.isValidElement(leftComponent) &&
+        leftComponent.type === AntDesign &&
+        leftComponent.props.name === 'arrowleft';
+
     return (
         <View style={styles.topBar}>
             {/* Left Component */}
-            <View style={styles.iconContainer}>
-                {leftComponent}
+            <View style={[styles.iconContainer, { flex: leftFlex }]}>
+                {isArrowIcon ? (
+                    <TouchableOpacity onPress={onLeftPress}>
+                        {leftComponent}
+                    </TouchableOpacity>
+                ) : (
+                    leftComponent
+                )}
             </View>
 
             {/* Center Component */}
-            <View style={[styles.iconContainer, iconContainerStyle]}>
+            <View style={[styles.iconContainer, iconContainerStyle, { flex: centerFlex }]}>
                 {centerComponent}
             </View>
 
             {/* Right Component */}
-            <View style={[styles.iconContainer, iconContainerStyle]}>
+            <View style={[styles.iconContainer, iconContainerStyle, { flex: rightFlex }]}>
                 {rightComponent}
             </View>
         </View>
@@ -30,6 +44,7 @@ const TopBar = ({
 
 const styles = StyleSheet.create({
     topBar: {
+        display: "flex",
         flexDirection: 'row',
         backgroundColor: '#b30000',
         height: 80, // Fixed height for the top bar
@@ -37,7 +52,6 @@ const styles = StyleSheet.create({
         width: '100%',
     },
     iconContainer: {
-        flex: 1, // Each container takes equal space
         justifyContent: 'center', // Center content vertically
         alignItems: 'center', // Center content horizontally
     },
